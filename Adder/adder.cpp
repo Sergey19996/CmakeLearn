2b2425e837837d54b91eb9c8a7891d1eb98a6bb6
Sergey19996 <kiriuchuksergey@gmail.com>
2025-04-13 11:55:28 +0300

    init

---
+++ Adder/adder.cpp
@@ -0,0 +1,12 @@
+#include "adder.h"
+int nearlymath::add(int a, int b){
+
+    return (a + b);
+}
+
+
+float nearlymath::add(float a, float b){
+
+return (a + b); 
+
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "adder.h"
+#include <iostream>
+int main(){
+std::cout<<"2 + 3  = " << nearlymath::add(2,3)<<'\n'<<std::endl;
+return EXIT_SUCCESS;
+
+}
